4990e3f9f9d8efd9fedd9740cc75133a3a7beb2a
bryonyoni <bryonyoni@gmail.com>
2020-01-19 23:06:54 +0300

    added a splash activity when launched

--- app/src/main/java/com/color/wordreader/Activities/SplashActivity.java
+++ app/src/main/java/com/color/wordreader/Activities/SplashActivity.java
@@ -2,7 +2,9 @@ package com.color.wordreader.Activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import com.color.wordreader.R;
 
@@ -12,5 +14,14 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
     }
 }
